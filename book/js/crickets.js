25c4ddb072ff5bfaef155f6d4be25e31b9a4b23b
Ed Millington <e.r.millingtondunelm.org.uk>
2011-11-19 23:27:48 +0000

    page 001 is coming along nicely.

---
+++ book/js/crickets.js
@@ -0,0 +1,248 @@
+jQuery(document).ready(function() {
+});
+
+
+jQuery(function() {
+
+
+var animating = false;
+jQuery('#wrapper002').click(function(e) {
+	if (animating) {
+		return;
+	}
+		
+	var title = jQuery('.titleCrickets');
+	var sub = jQuery('.subCrickets');
+	
+	
+	var one1 = jQuery('.one1');
+	var one2 = jQuery('.one2');
+	var one3 = jQuery('.one3');	
+	var one4 = jQuery('.one4');
+	var one5 = jQuery('.one5');
+	var one6 = jQuery('.one6');
+	var one7 = jQuery('.one7');
+	var one8 = jQuery('.one8');
+	var one9 = jQuery('.one9');
+	var one10 = jQuery('.one10');
+	var one11 = jQuery('.one11');
+	var one12 = jQuery('.one12');
+	var one13 = jQuery('.one13');
+	var one14 = jQuery('.one14');
+	
+	var two1 = jQuery('.two1');
+	var two2 = jQuery('.two2');
+	var two3 = jQuery('.two3');
+	var two4 = jQuery('.two4');
+	var two5 = jQuery('.two5');
+	var two6 = jQuery('.two6');
+	var two7 = jQuery('.two7');
+	var two8 = jQuery('.two8');
+	var two9 = jQuery('.two9');
+	var two10 = jQuery('.two10');
+	var two11 = jQuery('.two11');
+	var two12 = jQuery('.two12');
+	var two13 = jQuery('.two13');
+	
+	
+	title.addClass('disappear');
+	sub.addClass('disappear');
+	animating = true;
+	
+
+setTimeout(function() {
+setTimeout(function() {	
+		
+	one1.addClass('appear');
+	setTimeout(function() {
+		setTimeout(function() {
+			two1.addClass('appear');
+			setTimeout(function() {
+				setTimeout(function() {
+					one2.addClass('appear');
+				}, 1000);
+					setTimeout(function() {
+						setTimeout(function() {
+							two2.addClass('appear');							
+						}, 1000);
+						setTimeout(function() {
+							setTimeout(function() {
+								one3.addClass('appear');
+							}, 1000);
+							setTimeout(function() {
+								setTimeout(function() {
+									two3.addClass('appear');
+								}, 1000);
+									setTimeout(function() {
+										setTimeout(function() {
+											one4.addClass('appear');
+										}, 1000);
+											setTimeout(function() {
+												setTimeout(function() {
+													two4.addClass('appear');
+												}, 1000);
+													setTimeout(function() {
+														setTimeout(function() {
+															one5.addClass('appear');
+														}, 1000);
+															setTimeout(function() {
+																setTimeout(function() {
+																	two5.addClass('appear');
+																}, 1000);
+																	setTimeout(function() {
+																		setTimeout(function() {
+																			one6.addClass('appear');
+																		}, 1000);
+																			setTimeout(function() {
+																				setTimeout(function() {
+																					two6.addClass('appear');
+																				}, 1000);
+																				setTimeout(function() {
+																					setTimeout(function() {
+																						one7.addClass('appear');
+																					}, 1000);
+																					setTimeout(function() {
+																						setTimeout(function() {
+																							two7.addClass('appear');
+																						}, 1000);
+																							setTimeout(function() {
+																								setTimeout(function() {
+																									one8.addClass('appear');
+																								}, 1000);
+																								setTimeout(function() {
+																									setTimeout(function() {
+																										two8.addClass('appear');
+																									}, 1000);
+																									setTimeout(function() {
+																										setTimeout(function() {
+																											one9.addClass('appear');
+																										}, 1000);
+																										setTimeout(function() {
+																											setTimeout(function() {
+																												two9.addClass('appear');
+																											}, 1000);
+																											setTimeout(function() {
+																												setTimeout(function() {
+																													one10.addClass('appear');
+																												}, 1000);
+																												setTimeout(function() {
+																													setTimeout(function() {
+																														two10.addClass('appear');
+																													}, 1000);
+																													setTimeout(function() {
+																														setTimeout(function() {
+																															one11.addClass('appear');
+																														}, 1000);
+																															setTimeout(function() {
+																																setTimeout(function() {
+																																	two11.addClass('appear');
+																																}, 1000);
+																																	setTimeout(function() {
+																																		setTimeout(function() {
+																																			one12.addClass('appear');
+																																		}, 1000);
+																																			setTimeout(function() {
+																																				setTimeout(function() {
+																																					two12.addClass('appear');
+																																				}, 1000);
+																																					setTimeout(function() {
+																																						setTimeout(function() {
+																																							one13.addClass('appear');
+																																						}, 1000);
+																																							setTimeout(function() {
+																																								setTimeout(function() {
+																																									two13.addClass('appear');
+																																								}, 1000);
+																																								setTimeout(function() {
+																																									setTimeout(function() {
+																																										one14.addClass('appear');
+																																									}, 1000);
+																																									setTimeout(function() {
+																																										setTimeout(function() {
+																																											
+																																											title.removeClass('disappear');
+																																											title.addClass('appear');
+																																											sub.removeClass('disappear');
+																																											sub.addClass('appear');
+																																								
+																																											
+																																											one1.removeClass('appear');
+																																											one1.addClass('disappear');
+																																											one2.addClass('disappear');
+																																											one3.addClass('disappear');
+																																											one4.addClass('disappear');
+																																											one5.addClass('disappear');
+																																											one6.addClass('disappear');
+																																											one7.addClass('disappear');
+																																											one8.addClass('disappear');
+																																											one9.addClass('disappear');
+																																											one10.addClass('disappear');
+																																											one11.addClass('disappear');
+																																											one12.addClass('disappear');
+																																											one13.addClass('disappear');
+																																											one14.addClass('disappear');
+																																											two1.addClass('disappear');
+																																											two2.addClass('disappear');
+																																											two3.addClass('disappear');
+																																											two4.addClass('disappear');
+																																											two5.addClass('disappear');
+																																											two6.addClass('disappear');
+																																											two7.addClass('disappear');
+																																											two8.addClass('disappear');
+																																											two9.addClass('disappear');
+																																											two10.addClass('disappear');
+																																											two11.addClass('disappear');
+																																											two12.addClass('disappear');
+																																											two13.addClass('disappear');
+																																											
+																																											one1.removeClass('disapper');
+																																											one1.opacity = 0;
+																																											
+																																											animating = false;
+																																											
+																																																																																					
+																																											}, 1000);
+																																										
+																																						
+																																									}, 8000);
+																																								}, 3000);
+																																							}, 3000);
+																																					}, 3000);
+																																			}, 3000);
+																																	}, 3000);
+																															}, 3000);
+																													}, 3000);
+																												}, 3000);
+																											}, 3000);
+																										}, 3000);
+																									}, 3000);
+																								}, 3000);
+																							}, 3000);
+																					}, 3000);
+																				}, 3000);
+																			}, 3000);
+																	}, 3000);
+															}, 3000);
+													}, 3000);
+											}, 3000);
+									}, 3000);
+							}, 3000);
+						}, 3000);
+					}, 4000);
+			}, 2000);
+		}, 1000);
+	}, 4000);
+}, 1000);
+}, 4000);
+	
+	
+	e.preventDefault();
+});
+
+
+
+
+
+
+
+}); 
---
+++ book/js/hands.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function() {
+	
+	
+});
+
+
+jQuery(function() {
+
+
+	jQuery('#wrapper001').click(function(e) {
+
+	var handsBoy = jQuery('.handsBoy');
+	
+	handsBoy.addClass('boymove');
+
+	e.preventDefault();
+
+	});
+	
+
+});	
+
+
+
+
+
+
